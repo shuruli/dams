02503be776d8837ea9400eb3e11395f36e6317c1
David Chang <davidchang@v1020-wn-251-128.campus-dynamic.uwaterloo.ca>
2016-06-17 14:42:47 -0400

    project build

---
+++ DrakeTilt/desktop/src/com/draketilt/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.draketilt.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.draketilt.game.DrakeTilt;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DrakeTilt(), config);
+	}
+}
---
+++ DrakeTilt/html/src/com/draketilt/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.draketilt.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.draketilt.game.DrakeTilt;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new DrakeTilt();
+        }
+}
