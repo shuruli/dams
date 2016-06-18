b690ef9709e7944a10863f9b08b99fefdf7e66a5
Sudhanva Huruli <sshuruli@uwaterloo.ca>
2016-06-18 14:02:14 -0400

    initial falling objects

--- DrakeTilt/android/src/com/draketilt/game/AndroidLauncher.java
+++ DrakeTilt/android/src/com/draketilt/game/AndroidLauncher.java
@@ -10,7 +10,9 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		AndroidApplicationConfiguration config= new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
 		initialize(new DrakeTilt(), config);
 	}
 }
--- DrakeTilt/core/src/com/draketilt/game/DrakeTilt.java
+++ DrakeTilt/core/src/com/draketilt/game/DrakeTilt.java
@@ -3,25 +3,70 @@ package com.draketilt.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class DrakeTilt extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+    private Texture dropImage;
+    private Texture bucketImage;
+    private OrthographicCamera camera;
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
+    Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        Pixmap pixmap = new Pixmap( 64, 64, Pixmap.Format.RGBA8888 );
+        pixmap.setColor( 0, 1, 0, 0.75f );
+        pixmap.fillCircle( 32, 32, 32 );
+        dropImage = new Texture(pixmap);
+        raindrops = new Array<Rectangle>();
+        spawnFallingItems();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+
+        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnFallingItems();
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while(iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if(raindrop.y + 64 < 0) iter.remove();
+        }
+
+        batch.begin();
+        for(Rectangle raindrop: raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
+        batch.end();
+    }
+
+    private void spawnFallingItems() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800-64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    public void dispose(){
+        dropImage.dispose();
+        batch.dispose();
+    }
 }
